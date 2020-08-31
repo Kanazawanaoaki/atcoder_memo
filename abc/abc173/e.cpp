f36d9ee13b2c26225aa581b366016468b95b9c58
Kanazawanaoaki <naoaki65k@gmail.com>
2020-08-31 19:19:41 +0900

    add e

--- abc/abc173/e.cpp
+++ abc/abc173/e.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
-  
+  int n,k;cin >> n >> k;
 
+  vector<int> a(n);
+  for(int i=0;i<n;i++){
+    cin >> a[i];
+  }
+
+  for(int i=0;i<n;i++){
+    cout << a[i] << " ";
+  }
+
+  cout << endl;
   return 0;
 }
